03f6b40f9eac9f0e82f752e4592f0e841f01c10e
Mateus Nadal <nadalmateus@hotmail.com>
2021-06-24 14:06:51 -0300

    gitignore

---
+++ efcore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace efcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
