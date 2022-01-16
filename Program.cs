80edf4d624b079069db7306a02ea734b1abdc797
Mateus Nadal <nadalmateus@hotmail.com>
2022-01-16 20:34:31 -0300

    feat: added asnotracking() in get methods

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿using Blog.Data;
+﻿using System;
+using System.Linq;
+using Blog.Data;
 using Blog.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog
 {
@@ -9,9 +12,34 @@ namespace Blog
         {
             using var context = new BlogDataContext();
             {
-                var tag = new Tag { Name = "ASP.NET", Slug = "asp.net" };
-                context.Tags.Add(tag);
-                context.SaveChanges();
+                //CREATE
+                // var tag = new Tag { Name = ".NET", Slug = "dotnet" };
+                // context.Tags.Add(tag);
+                // context.SaveChanges();
+
+                // UPDATE
+                // var tag = context.Tags.FirstOrDefault(x => x.Id == 4);
+                // tag.Name = "ASP.NET";
+                // tag.Slug = "EF Core";
+                // context.Update(tag);
+                // context.SaveChanges();
+
+                //DELETE
+                // var tag = context.Tags.FirstOrDefault(x => x.Id == 3);
+                // context.Tags.Remove(tag);
+                // context.SaveChanges();
+
+                // //GET
+                // var tags = context.Tags.AsNoTracking().Where(x => x.Name.Contains(".NET")).ToList();
+
+                // foreach (var tag in tags)
+                // {
+                //     Console.Write(tag.Name);
+                // }
+
+                var tag = context.Tags.AsNoTracking().FirstOrDefault(x => x.Id == 1);
+                Console.WriteLine(tag?.Name);
+
             }
         }
     }
