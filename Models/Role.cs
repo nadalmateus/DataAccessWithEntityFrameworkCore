dc7a70c9d795888e592e9f70d65e3c78210bc96b
Mateus Nadal <nadalmateus@hotmail.com>
2022-01-16 17:45:01 -0300

    feat: added role model

---
+++ Models/Role.cs
@@ -0,0 +1,9 @@
+namespace Blog.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public string Slug { get; set; }
+    }
+}
