f4478257cf2aaa89cc9a4559c595d2b92824627e
Mateus Nadal <nadalmateus@hotmail.com>
2022-01-16 17:53:38 -0300

    feat: added anothers models

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,6 +1,6 @@
 namespace Blog.Models
 {
-    public class Role
+    public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; }
--- Models/Tag.cs
+++ Models/Tag.cs
@@ -1,6 +1,6 @@
 namespace Blog.Models
 {
-    public class Role
+    public class Tag
     {
         public int Id { get; set; }
         public string Name { get; set; }
