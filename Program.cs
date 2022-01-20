1c80e70c2a7c016016b44cc55bf2096748982d9d
Mateus Nadal <nadalmateus@hotmail.com>
2022-01-19 21:50:41 -0300

    feat: added first migration

--- Program.cs
+++ Program.cs
@@ -1,44 +1,10 @@
-﻿using System;
-using System.Linq;
-using Blog.Data;
-using Blog.Models;
-
-namespace Blog
+﻿namespace Blog
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            using var context = new BlogDataContext();
 
-            // context.Users.Add(new User
-            // {
-            //     Bio = "Software Engineer",
-            //     Email = "mateus@nadal.com",
-            //     Image = "https://localhost/image",
-            //     Name = "Mateus Nadal",
-            //     PasswordHash = "123123123123123",
-            //     Slug = "mateusnadal"
-            // });
-            // context.SaveChanges();
-
-            var user = context.Users.FirstOrDefault();
-            var post = new Post
-            {
-                Author = user,
-                Body = ".NET",
-                Category = new Category
-                {
-                    Name = "Back-end",
-                    Slug = "backend"
-                },
-                CreateDate = DateTime.Now,
-                Slug = "dotnet",
-                Summary = "Learn .NET",
-                Title = ".NET",
-            };
-            context.Posts.Add(post);
-            context.SaveChanges();
         }
     }
 }
