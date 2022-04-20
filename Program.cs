859f780143220a5e89cf39daa17858cf0e151557
Mateus Nadal <nadalmateus@hotmail.com>
2022-04-19 22:01:58 -0300

    feat: added a create and a update function

--- Program.cs
+++ Program.cs
@@ -4,5 +4,13 @@ Console.WriteLine("Hello, World!");
 
 using (var context = new BlogDataContext())
 {
+    // var tag = new Tag { Name = "ASP.NET", Slug = "aspnet" };
+    // context.Tags.Add(tag);
+    // context.SaveChanges();
 
+    var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+    tag.Name = ".NET";
+    tag.Slug = "dotnet";
+    context.Update(tag);
+    context.SaveChanges();
 }
