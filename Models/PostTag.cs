cba6b2b8acba6ad95dd3a1de75d3ba951dc6da37
Mateus Nadal <nadalmateus@hotmail.com>
2022-04-19 21:27:14 -0300

    feat: project setup

---
+++ Models/PostTag.cs
@@ -0,0 +1,6 @@
+namespace Blog.Model;
+public class PostTag
+{
+    public int PostId { get; set; }
+    public int TagId { get; set; }
+}
---
+++ Models/UserRole.cs
@@ -0,0 +1,6 @@
+namespace Blog.Model;
+public class UserRole
+{
+    public int UserId { get; set; }
+    public int RoleId { get; set; }
+}
